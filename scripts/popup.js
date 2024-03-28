59605b5a46daf79a145ee3ddb859a7ff476a97e3
sivani-l-r <sivanilr03@gmail.com>
2024-03-28 19:33:41 +0530

    date picker added

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,47 +4,46 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     document.getElementById("currentWebsite").innerText = "Current website: " + currentUrl;
 });
 
-
 document.addEventListener("DOMContentLoaded", function () {
     var submitBtn = document.getElementById("submitBtn");
     var successMessage = document.getElementById("userSuccessMessage");
+    var reminderDate = document.getElementById("datePicker");
   
     submitBtn.addEventListener("click", function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currentTab = tabs[0];
-        var currentUrl = currentTab.url;
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            var currentTab = tabs[0];
+            var currentUrl = currentTab.url;
+
+            var formattedReminderDate = reminderDate.value; 
   
-        chrome.bookmarks.search({ title: "Bookmark Reminder Extension" }, function (results) {
-          if (results.length > 0) {
-            console.log(results);
-            var existingFolder = results[0];
-            chrome.bookmarks.create({
-              parentId: existingFolder.id,
-              title: currentTab.title,
-              url: currentUrl
-            }, function (newBookmark) {
-              console.log("Bookmark saved:", newBookmark);
-              successMessage.innerText = "Bookmark saved successfully to: \nFolder - " + existingFolder.title + "\nTab - " + currentTab.title;
+            chrome.bookmarks.search({ title: "Bookmark Reminder Extension" }, function (results) {
+                if (results.length > 0) {
+                    var existingFolder = results[0];
+                    chrome.bookmarks.create({
+                        parentId: existingFolder.id,
+                        title: currentTab.title + ' - ' + formattedReminderDate, 
+                        url: currentUrl,
+                    }, function (newBookmark) {
+                        console.log("Bookmark saved:", newBookmark);
+                        successMessage.innerText = "Bookmark saved successfully to: \nFolder - " + existingFolder.title + "\nTab - " + currentTab.title;
+                    });
+                } else {
+                    chrome.bookmarks.create({
+                        title: "Bookmark Reminder Extension"
+                    }, function (newFolder) {
+                        console.log("Bookmark folder created:", newFolder);
+                        
+                        chrome.bookmarks.create({
+                            parentId: newFolder.id,
+                            title: currentTab.title + ' - ' + formattedReminderDate, 
+                            url: currentUrl,
+                        }, function (newBookmark) {
+                            console.log("Bookmark saved:", newBookmark);
+                            successMessage.innerText = "Bookmark saved successfully to: \nFolder - " + newFolder.title + "\nTab - " + currentTab.title;
+                        });
+                    });
+                }
             });
-          } else {
-            chrome.bookmarks.create({
-              title: "Bookmark Reminder Extension"
-            }, function (newFolder) {
-              console.log("Bookmark folder created:", newFolder);
-  
-              
-              chrome.bookmarks.create({
-                parentId: newFolder.id,
-                title: currentTab.title,
-                url: currentUrl
-              }, function (newBookmark) {
-                console.log("Bookmark saved:", newBookmark);
-                successMessage.innerText = "Bookmark saved successfully to: \nFolder - " + newFolder.title + "\nTab - " + currentTab.title;
-              });
-            });
-          }
         });
-      });
     });
-  });
-  
+});
